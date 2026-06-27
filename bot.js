491a1853b9bef1bce6eb0d5ace58f5186eac0b07
Kyale <geffreyjay@gmail.com>
2026-06-27 21:24:43 +0300

    change commit bal

--- bot.js
+++ bot.js
@@ -122,7 +122,7 @@ const CONFIG = {
   },
 
   PAPER: {
-    startingBalance: Number(process.env.STARTING_BALANCE || 100),
+    startingBalance: Number(process.env.STARTING_BALANCE || 10),
     stakeUsd: Number(process.env.STAKE_USD || 1),
     feeBps: Number(process.env.FEE_BPS || 0),
   },
